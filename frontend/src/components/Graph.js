b03207671f825a6ed5ef053afe4a0e57f226459e
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2019-03-09 19:42:23 +0100

    Fix pie chart data reference
    Add a color for each emotion
    Increased spacing

--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -4,43 +4,43 @@ class Expressions extends Component {
 	function refreshGraph(people, index) {
 		var data = people[index].getPieData();
 
-		var w=300,h=300;
-		var radius=(w-20)/2;
+		var w = 300,h = 300;
+		var radius = (w - 20)/2;
 		 
-		var pie=d3.layout.pie()
-			.value(function(d){return d.percent})
+		var pie = d3.layout.pie()
+			.value(function(d) { return d.value })
 			.sort(null);
-		var arc=d3.svg.arc()
+		var arc = d3.svg.arc()
 			.innerRadius(0)
 			.outerRadius(radius);
 		var color = d3.scale.ordinal()
-			.range([ '#e75244','#33bb9d']);
-		var svg=d3.select("#chart")
+			.range(['#ff0000', '#ffaa00', '#009900', '#ffff00', '#00ff00', '#dddddd', '#0000ff', '#00ffff']);
+		var svg = d3.select("#chart")
 			.append("svg")
 			.attr({
-				width:w,
-				height:h,
-				class:'shadow'
+				width: w,
+				height: h,
+				class: 'shadow'
 			}).append('g')
-			.attr('transform','translate('+(w/2)+','+(h/2)+')');
-		var path=svg.selectAll('path')
+			.attr('transform', 'translate(' + (w / 2) + ',' + (h / 2) + ')');
+		var path = svg.selectAll('path')
 			.data(pie(data))
 			.enter()
 			.append('path')
 			.attr({
-				d:arc,
-				fill:function(d,i){
-				return color(i);
+				d: arc,
+				fill: function(d, i){
+					return color(i)
 				}
 			})
 			.style({
-				'fill-opacity':.15,
-				stroke: function(d,i){
-				return color(i);
+				'fill-opacity': 0.15,
+				stroke: function(d, i){
+					return color(i)
 				},
 				'stroke-width': '2px'
 			});
-		var text=svg.selectAll('text')
+		var text = svg.selectAll('text')
 			.data(pie(data))
 			.enter()
 			.append("text")
@@ -48,14 +48,14 @@ class Expressions extends Component {
 				return "translate(" + arc.centroid(d) + ")";
 			})
 			.attr("text-anchor", "middle")
-			.text(function(d){
-				return d.data.name+" ("+d.data.percent+"%)" ;
+			.text(function(d) {
+				return d.data.name + " (" + d.data.percent + "%)"
 			})
 			.style({
-				fill:function(d,i){
-				return color(i);
+				fill:function(d, i) {
+					return color(i)
 				},
-				'font-size':'18px',
+				'font-size': '18px',
 		 
 			});
 	}
@@ -63,6 +63,7 @@ class Expressions extends Component {
 	render() { 
 		return (
 			const { people } = this.props;
+			<script src="https://d3js.org/d3.v5.min.js"></script>
 			<div class='widget'>
 				<div class='header'>Emotional Distribution</div>
 				<div id='chart' class='chart-container'></div>
