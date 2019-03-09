2147e9918147fe8425f946b523e1d3f250db0552
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2019-03-09 02:57:07 +0100

    Can rename people
    Person class tracks emotion data as dictionary and generates vector as needed
    Use proper JSON data for testing

--- backend/feedback.py
+++ backend/feedback.py
@@ -6,20 +6,37 @@ class FeedbackModule:
 	people = {}
 
 	@staticmethod
+	def rename_person(faceID, newName):
+		FeedbackModule.people[faceID].name = newName
+
+	@staticmethod
+	def get_people():
+		return [(ID, person.name) for ID, person in FeedbackModule.people.items()]
+
+	@staticmethod
+	def force_feedback():
+		return "\n".join([FeedbackModule.get_feedback(person, person.lastEmotion) for person in FeedbackModule.people.values()])
+
+	@staticmethod
 	def get_feedback(person, emotion):
-		primary = FeedbackModule.get_top_emotions(emotion)[-1]
+		topEmotions = FeedbackModule.get_top_emotions(emotion)
+		if len(topEmotions) == 0:
+			return "No emotions detected"
+		primary = topEmotions[-1]
+		# Update last emotion
+		person.lastEmotion = emotion
 		# Preliminary textual feedback
 		return "{} is {} percent {}".format(person.name, primary[1] * 100, primary[0])
-		person.lastEmotion = emotion
 
 	@staticmethod
 	def load_new_emotion(face):
 		if face["faceId"] not in FeedbackModule.people:
 			FeedbackModule.people[face["faceId"]] = Person(face["faceId"])
 		person = FeedbackModule.people[face["faceId"]]
-		newEmotion = np.array([value for _, value in face["faceAttributes"]["emotion"].items()])
-		if np.linalg.norm(newEmotion - person.lastEmotion) > Backend.get_emotion_change_threshold():
-			return FeedbackModule.get_feedback(person, face["faceAttributes"]["emotion"])
+		newEmoData = face["faceAttributes"]["emotion"]
+		newEmotion = np.array([value for _, value in newEmoData.items()])
+		if np.linalg.norm(newEmotion - person.get_emotion_vector()) > Backend.get_emotion_change_threshold():
+			return FeedbackModule.get_feedback(person, newEmoData)
 		return None
 
 	@staticmethod
